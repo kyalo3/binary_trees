377f1ada49094324534f2f645b9987c5a1e13ae6
Kiruti01 <mikekirutic@gmail.com>
2023-10-30 14:57:33 +0300

    task 1

--- binary_trees.h
+++ binary_trees.h
@@ -9,6 +9,25 @@
 /* Data structures */
 
 /**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/**
  * struct levelorder_queue_s - Level order traversal queue.
  * @node: A node of a binary tree.
  * @next: The next node to traverse to in the binary tree.
