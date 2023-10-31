6516379c7aaf9ce533eab14736388d01c0988861
kyalo3 <kyalokimeu0@gmail.com>
2023-10-31 12:24:35 +0300

    12. Leaves Write a function that counts the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -18,5 +18,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (1);
 	}
 
-	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
