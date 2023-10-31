195d447eefda1a0eadf8374ca5e65aec50cf7789
kyalo3 <kyalokimeu0@gmail.com>
2023-10-31 13:20:27 +0300

    14. Balance factor Write a function that measures the balance factor of a binary tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balace factor of  binary tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: If tree is NULL, the function must return 0, else balance.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL || tree->parent != NULL)
+	{
+		return (0);
+	}
+
+	return (1);
+}
