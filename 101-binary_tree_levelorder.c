0dc47d841b8164624b5a12ce083c76ba6392008e
kyalo3 <kyalokimeu0@gmail.com>
2023-11-02 16:12:14 +0300

    20. Level-order traversal Write a function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -11,26 +11,54 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1000);
+	levelorder_queue_t *queue = NULL;
 
+	queue = malloc(sizeof(levelorder_queue_t));
 	if (queue == NULL)
 		return;
 
-	int front = 0, rear = 0;
+	queue->node = (binary_tree_t *)tree;
+	queue->next = NULL;
 
-	queue[rear++] = (binary_tree_t *)tree;
-
-	while (front < rear)
+	while (queue != NULL)
 	{
-		binary_tree_t *current = queue[front++];
+		binary_tree_t *current = queue->node;
 
 		func(current->n);
 
 		if (current->left != NULL)
-			queue[rear++] = current->left;
+		{
+			levelorder_queue_t *new_node = malloc(sizeof(levelorder_queue_t));
+			if (new_node == NULL)
+				return;
+
+			new_node->node = current->left;
+			new_node->next = NULL;
+
+			levelorder_queue_t *temp = queue;
+			while (temp->next != NULL)
+				temp = temp->next;
+
+			temp->next = new_node;
+		}
 
 		if (current->right != NULL)
-			queue[rear++] = current->right;
+		{
+			levelorder_queue_t *new_node = malloc(sizeof(levelorder_queue_t));
+			if (new_node == NULL)
+				return;
+
+			new_node->node = current->right;
+			new_node->next = NULL;
+
+			levelorder_queue_t *temp = queue;
+			while (temp->next != NULL)
+				temp = temp->next;
+
+			temp->next = new_node;
+		}
+		levelorder_queue_t *temp = queue;
+		queue = queue->next;
+		free(temp);
 	}
-	free(queue);
 }
